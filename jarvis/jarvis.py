e1ba5689ca59e3ddf5a073864df3d6afb0354093
Deepanshu-Rohilla <52520071+Deepanshu-Rohilla@users.noreply.github.com>
2020-04-28 22:47:58 +0530

    Add files via upload

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -4,6 +4,9 @@ import os
 import time
 import playsound
 from gtts import gTTS
+import speech_recognition as sr
+import pyaudio
+
 
 
 #engine = pyttsx3.init('sapi5')
@@ -11,11 +14,13 @@ from gtts import gTTS
 #print(voices)
 
 def speak(text):
-    tts = gTTS(text = text, lang = "en")
+    tts = gTTS(text = text, lang = "en-in")
     filename = "voice.mp3"
     tts.save(filename)
     playsound.playsound(filename)
 def greetMe():
+
+    # it will run in the starting
     hour = int(datetime.datetime.now().hour)
     if(hour>=0 and hour<12):
         speak("Good morning Deepanshu sir. How may I help you?")
@@ -23,6 +28,21 @@ def greetMe():
         speak("Good afternoon Deepanshu sir. How may I help you?")
     else:
         speak("Good evening Deepanshu sir. How may I help you?")
-
+def takeCommand():
+    #It takes microphone input from user and returns string output
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language="en-in")
+        print("User said: ", (query))
+    except:
+        print("Say that again please...")
+        return "None"
+    
+    return query
 if __name__ == "__main__":
     greetMe()
+    takeCommand()
