9bc18f4720a98f31b40ec7eddc81f1b9c4ae9665
Deepanshu-Rohilla <52520071+Deepanshu-Rohilla@users.noreply.github.com>
2020-04-29 15:22:48 +0530

    Play songs on spotify and also send emails.
    now you can ask it to play any song for you from spotify. But you must have spotify installed. Also, you can send emails...

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -7,12 +7,10 @@ from gtts import gTTS
 import speech_recognition as sr
 import pyaudio
 import wikipedia
-
-
-#engine = pyttsx3.init('sapi5')
-#voice = engine.getProperty('voices')
-#print(voices)
-
+import webbrowser
+import sys
+import subprocess
+import smtplib
 def speak(text):
     tts = gTTS(text = text, lang = "en-in")
     filename = "voice.mp3"
@@ -37,16 +35,28 @@ def takeCommand():
     try:
         print("Recognizing...")
         query = r.recognize_google(audio, language="en-in")
-        print("User said: ", (query))
+        print(query)
     except:
         print("Say that again please...")
         return "None"
-    return query
+    return query    
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login("youremail@gmail.com", "your-password")
+    # youremail@gmail.com => plug in your emailid here
+    #your-password => plug in your password here
+    server.sendmail("youremail@gmail.com", to, content)
+    server.close()
+
+
+
 if __name__ == "__main__":
     greetMe()
 
     #logic here
-    while(True):
+    if(1):
         query = takeCommand().lower()
         if('wikipedia' in query):
             speak("Searching wikipedia...")
@@ -54,4 +64,32 @@ if __name__ == "__main__":
             results= wikipedia.summary(query, sentences=2)
             speak("According to wikipedia")
             speak(results)
+        elif ("youtube" in query):
+            print("here")
+            webbrowser.open("http://www.youtube.com")
+        elif ("open" in query and "google" in query):
+            webbrowser.open("http://www.google.com")
+        elif ("stackoverflow" in query):
+            webbrowser.open("http://www.stackoverflow.com")
+        elif("p***" in query): #if someone asks it to open porn ( speech recognition convets porn to p*** in string query)
+            speak("abey saale tharki")
+        elif (("time" in query) and ("what" in query)):
+            t = datetime.datetime.now().strftime("%H:%M")
+            speak("Sir, the time is " + t )
+        elif("spotify" in query):
+           # os.system(Applications/Spotify)
+            opener ="open" if sys.platform == "darwin" else "xdg-open"
+            subprocess.call([opener, "/Applications/Spotify" ])
+        elif("email to deepanshu"):
+            try:
+                speak("What should I say")
+                content = takeCommand()
+                to = reciever@gmail.com 
+                # replace reciever@gmail.com with the sender's email
+                sendEmail(to, content)
+                speak("Your email has been sent")
+            except:
+                speak("Sorry yaar. Nahi ho paaya. Pata nahi kya ho gaya")
+    
 
+            
